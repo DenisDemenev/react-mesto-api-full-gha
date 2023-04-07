1ac00a433e652c90c01e21fd9ea2180c06e1fbe3
DenisDemenev <lazer89@list.ru>
2023-04-07 19:23:44 +0500

    test

--- backend/middlewares/validation.js
+++ backend/middlewares/validation.js
@@ -40,7 +40,7 @@ module.exports.signUpValidation = celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().required().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+    avatar: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
   }),
 });
 
